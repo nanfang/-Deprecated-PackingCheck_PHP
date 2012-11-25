f4a3f5a7f9ae6b994a2a49844835ee6c7047974a
Fang Nan <nanfang05@gmail.com>
2012-11-25 11:55:27 +0800

    add bootswatch files

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Packing Check</title>
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
+
+</head>
+<body>
+<h1><?php echo "Hello, world \n";?></h1>
+
+
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
+</body>
+</html>

c78aa9148858eb414d9bd5b9fcf9bff8fd75915f
Fang Nan <nanfang05@gmail.com>
2012-12-12 23:24:03 +0800

    introduce ti.php for layout template

---
+++ www/base.php
@@ -0,0 +1,41 @@
+<?php require_once 'ti.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Packing Check</title>
+    <meta charset="UTF-8">
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 60px;
+        }
+    </style>
+    <?php startblock('title') ?>
+    <?php endblock() ?>
+</head>
+<body>
+<div class="navbar  navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="brand" href="/">PackingCheck</a>
+            <div class="nav-collapse collapse pull-right">
+                <ul class="nav">
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Sign Out</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
+
+<div class="container">
+<?php startblock('content') ?>
+<?php endblock() ?>
+</div>
+<!-- /container -->
+
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ www/items.php
@@ -0,0 +1,16 @@
+<?php include 'base.php' ?>
+<?php startblock('content') ?>
+<h1>Go Hiking</h1>
+<p>
+    <button class="btn btn-large btn-primary">
+        Add Item
+    </button>
+</p>
+<ul>
+    <li>Alpenstock</li>
+    <li>Sports drink</li>
+    <li>TrekkingShoes</li>
+</ul>
+
+<?php endblock() ?>
+
